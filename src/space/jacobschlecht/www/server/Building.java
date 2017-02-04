560947e5d6f191e1e8b556698392294f2059ee1c
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2017-02-03 21:52:58 -0600

    Added an ENUM for building type

--- src/space/jacobschlecht/www/server/Building.java
+++ src/space/jacobschlecht/www/server/Building.java
@@ -2,8 +2,19 @@ package space.jacobschlecht.www.server;
 
 public class Building extends Room {
 
+	public enum buildingType {
+		BLACKSMITH, WEAPONSHOP, ARMORSHOP, ENCHANTERY, LIBRARY, TOWN_HALL, DEFAULT
+	}
+	
+	buildingType type;
+	
 	public Building(String name) {
+		this(name, buildingType.DEFAULT);
+	}
+	
+	public Building(String name, buildingType type) {
 		super(name);
+		this.type = type;
 	}
 	
 	public boolean isBuilding() {
